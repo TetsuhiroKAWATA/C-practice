04f715d022bf069675e27a4f6ab9e1333ba9d466
TetsuhiroKAWATA <philo.cosmo717@gmail.com>
2022-01-22 01:23:21 +0900

    ちゃんとプッシュできてますか？

---
+++ 1-1/1-1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>//iostreamはある場所のファイルの名前を指定するものらしい。
+
+using namespace std;//ネームスペースの設定らしい
+
+int main()
+{
+    cout << "Hello World!\n";//Console OUTputらしい
+    /*ネームスペースがない時の記述
+    std::cout << "Hello World!|n";*/
+
+    return 0;
+}
