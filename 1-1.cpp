16fff3fb11000c2e59fe5006823496106930c9be
TetsuhiroKAWATA <philo.cosmo717@gmail.com>
2022-01-22 01:10:45 +0900

    コミットやってみる

--- 1-1.cpp
+++ 1-1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>//iostreamはある場所のファイルの名前を指定するものらしい。
 
-using namespace std;
+using namespace std;//ネームスペースの設定らしい
 
 int main()
 {
     cout << "Hello\n";
+    /*ネームスペースがない時の記述
+    std::cout << "Hello|n";*/
 
     return 0;
 }
